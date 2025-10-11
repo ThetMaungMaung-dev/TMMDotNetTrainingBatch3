006dd6068b28eb0a7084ea6a4adce239bf488b27
Thet Maung Maung <thetmgmg129@gmail.com>
2025-10-11 14:39:03 +0630

    feat : ado.net read

--- TMMDotNetTrainingBatch3.Console.App/Program.cs
+++ TMMDotNetTrainingBatch3.Console.App/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+using System.Diagnostics;
+
+string name = "ATE";
+
+if (name == "A")
+{
+    
+}
+else
+{
+
+}
+
+enumBank LL = enumBank.CB;
+switch (LL)
+{
+    case enumBank.AYA:
+        Console.WriteLine("A");
+        break;
+}
+enum enumBank
+{
+    None = 0,
+    KBZ = 1,
+    CB = 2,
+    AYA = 3,
+    MAB = 4
+}
---
+++ TMMDotNetTrainingBatch3.ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+//Console.WriteLine("Hello, World!");
+SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+sqlConnectionStringBuilder.DataSource = "."; //servername
+sqlConnectionStringBuilder.InitialCatalog = "Batch3MiniPOS"; //Databasename
+sqlConnectionStringBuilder.UserID = "sa";//Username
+sqlConnectionStringBuilder.Password = "sasa@123";//Password
+sqlConnectionStringBuilder.TrustServerCertificate = true;//
+
+SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+connection.Open();
+
+string query = @"SELECT [ProductId]
+      ,[ProductName]
+      ,[Price]
+      ,[DeleteFlag]
+  FROM [dbo].[Tbl_Product]";
+
+SqlCommand command = new SqlCommand(query, connection);
+SqlDataAdapter adapter = new SqlDataAdapter(command);
+DataTable dataTable = new DataTable();
+adapter.Fill(dataTable);
+
+connection.Close();
+
+for (int i = 0;i < dataTable.Rows.Count; i++)
+{
+    DataRow row = dataTable.Rows[i];
+    //Console.WriteLine(row["ProductId"]);
+    int rownumber = i + 1;
+    decimal price = Convert.ToDecimal(row["Price"]);
+    Console.WriteLine(rownumber.ToString() + ". " + row["ProductName"] + "(" + price.ToString("n0")+ ")");
+    //Console.WriteLine(rownumber.ToString() + ". " + row["ProductName"] + "(" + row["Price"] + ")");
+
+    //Console.WriteLine("Price >>>>" + row["Price"]);
+    //Console.WriteLine("----------------------------------");
+}
+Console.ReadLine();
